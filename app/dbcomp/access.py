393294bca11b0cdd1cb5bb0c9f0f90e91e4de952
Carlos Mongenegro <cgmonte@gmail.com>
2021-02-19 20:50:30 +0000

    novas credenciais de acesso db e configuração do gitignore

--- app/dbcomp/access.py
+++ app/dbcomp/access.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dbcomp import dbconfig
 
 #SETUP DB APP
-engine_app_db = create_engine("postgresql://micros-gdrive:pw2dbIvfVt9JB+1eXAEyJrN+PkvquHaVnSDtXInRcuk=@pbla_db_1/micros-gdrive-app")
+engine_app_db = create_engine(f"postgresql://micros-gdrive:{dbconfig.password}@pbla_db_1/micros-gdrive-app")
 SessionLocalA = sessionmaker(autocommit=False, autoflush=False, bind=engine_app_db)
 BaseA = declarative_base()
 BaseA.metadata.create_all(bind=engine_app_db)
@@ -16,7 +17,7 @@ def get_app_db():
         db.close()
 
 #SETUP DB DATA
-engine_data_db = create_engine("postgresql://micros-gdrive:pw2dbIvfVt9JB+1eXAEyJrN+PkvquHaVnSDtXInRcuk=@pbla_db_1/micros-gdrive-data")
+engine_data_db = create_engine(f"postgresql://micros-gdrive:{dbconfig.password}@pbla_db_1/micros-gdrive-data")
 SessionLocalB = sessionmaker(autocommit=False, autoflush=False, bind=engine_data_db)
 BaseB = declarative_base()
 # BaseB.metadata.create_all(bind=engine_data_db)
